480859b81dbcb1ce9a734f3d8eee255defcf4dc9
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-02-13 11:44:40 +0100

    Create simd_test.jl

---
+++ test/simd_test.jl
@@ -0,0 +1,46 @@
+function init!(u::Vector)
+    n = length(u)
+    dx = 1.0 / (n-1)
+    @fastmath @inbounds @simd for i in 1:n #by asserting that `u` is a `Vector` we can assume it has 1-based indexing
+        u[i] = sin(2pi*dx*i)
+    end
+end
+
+function deriv!(u::Vector, du)
+    n = length(u)
+    dx = 1.0 / (n-1)
+    @fastmath @inbounds du[1] = (u[2] - u[1]) / dx
+    @fastmath @inbounds @simd for i in 2:n-1
+        du[i] = (u[i+1] - u[i-1]) / (2*dx)
+    end
+    @fastmath @inbounds du[n] = (u[n] - u[n-1]) / dx
+end
+
+function mynorm(u::Vector)
+    n = length(u)
+    T = eltype(u)
+    s = zero(T)
+    @fastmath @inbounds @simd for i in 1:n
+        s += u[i]^2
+    end
+    @fastmath @inbounds return sqrt(s)
+end
+
+function main()
+    n = 2000
+    u = Vector{Float64}(undef, n)
+    init!(u)
+    du = similar(u)
+
+    deriv!(u, du)
+    nu = mynorm(du)
+
+    @time for i in 1:10^6
+        deriv!(u, du)
+        nu = mynorm(du)
+    end
+
+    println(nu)
+end
+
+main()
